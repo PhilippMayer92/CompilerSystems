1ddf682486919a59356f80c079ca63d17f9180d5
Philipp Mayer <mayerph@stud.sbg.ac.at>
2017-03-09 21:38:08 +0100

    erster countdown-test

--- countdown.c
+++ countdown.c
@@ -1,3 +1,27 @@
+int counter=0;
+
+int countdown_at_idx(int* ptr, int idx){
+	int numb=*(ptr+idx);
+	int red;
+	if(numb%2==0){
+		red=2;
+	}else{
+		red=1;
+	}
+	while(numb>0){
+		numb=numb-red;
+		counter=counter+1;
+	}
+	
+}
+
 int main(){
-	d
+	int i=0;
+	int* array=malloc(20*4);
+	while(i<20){
+		*(array+i)=i;
+		countdown_at_idx(array, i);
+		i=i+1;
+	}
+	return counter;
 }
